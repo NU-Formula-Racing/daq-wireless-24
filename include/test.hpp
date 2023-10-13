6e58799a12c2177a9adae332c1531fceeb2cacc8
Shrimp-Chip <esample21@gmail.com>
2023-10-12 20:16:22 -0500

    Implement TestClass

---
+++ include/test.hpp
@@ -0,0 +1,13 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+class TestClass
+{
+    public:
+        TestClass() {}
+        ~TestClass() {}
+
+        void print();
+};
+
+#endif // __TEST_H__
---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include "test.hpp"
+
+#include <iostream>
+
+void TestClass::print()
+{
+    std::cout << "Hello World!" << std::endl;
+}
