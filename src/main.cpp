bcf02e8d2ee5104c8751974043fd8b3ecbb4df54
Shrimp-Chip <esample21@gmail.com>
2024-05-08 10:01:12 -0500

    Began sketching out firmware for testing daqser library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
+#include <daqser.hpp>
+#include <daqser_can.hpp>
 
-// setup function
+#define VERSION_ARGS(major, minor, patch) major, minor, patch
+
+#define SCHEMA_NAME "test_schema_v1.0.0.drive"
+#define SCHEMA_VERSION VERSION_ARGS(1, 0, 0)
+
+// Listen to boards
+#define RX_BMS
+#define RX_IMD
+
+VirtualTimerGroup g_timerGroup;
+// Function to record and send data
+// Called every 100ms
+void recordData()
+{
+  //  Prepare data from the CAN bus for serialization
+  daqser::updateSignals();
+  // Serialize the data, and get the raw bytes
+  std::vector<std::uint8_t> byteData = daqser::serializeFrame();
+  // send the data over lora or something
+  // send(byteData);
+}
+
 void setup()
 {
-  // setup
+  Serial.begin(115200);
+  // Initialize the daqser library
+  daqser::initialize();
+  // Tell daqser what schema we are using to serialize the data
+  daqser::setSchema(SCHEMA_NAME, SCHEMA_VERSION);
+  // Tell daqser to record/send data from these boards
+  g_timerGroup.AddTimer(100, recordData);
 }
 
 void loop()
 {
-  // loop
+  g_timerGroup.Tick(millis());
 }
