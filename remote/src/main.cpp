f15bc009dd51847a470a7a31c647c8be822f47e5
Shrimp-Chip <esample21@gmail.com>
2024-05-26 19:52:04 -0500

    Some changes

--- remote/src/main.cpp
+++ remote/src/main.cpp
@@ -1,43 +1,32 @@
 #include <Arduino.h>
 #include <daqser.hpp>
-// #include <daqser_can.hpp>
 #include <virtualTimer.h>
-#include <teensy_can.h>
+#include <message.hpp>
+#include <com_interface.hpp>
+#include <RH_RF95.h> // dependency of com_interface.hpp, for some reason, it isn't included in the main.cpp file
+
+#include <vector>
 
 #define VERSION_ARGS(major, minor, patch) major, minor, patch
-
-#define SCHEMA_NAME "test_schema_v1.0.0.drive"
+#define SCHEMA_NAME "esp32dev_test"
 #define SCHEMA_VERSION VERSION_ARGS(1, 0, 0)
 
-// Listen to boards
-// #define RX_BMS
-// #define RX_IMD
+wircom::ComInterface g_comInterface{};
 
-VirtualTimerGroup timerGroup;
-// Function to record and send data
-// Called every 100ms
-void recordData()
-{
-  //  Prepare data from the CAN bus for serialization
-  // daqser::updateSignals();
-  // Serialize the data, and get the raw bytes
-  std::vector<std::uint8_t> byteData = daqser::serializeFrame();
-  // send the data over lora or something
-  // send(byteData);
-}
 
 void setup()
 {
-  Serial.begin(115200);
-  // Initialize the daqser library
-  daqser::initialize();
-  // Tell daqser what schema we are using to serialize the data
-  daqser::setSchema(SCHEMA_NAME, SCHEMA_VERSION);
-  // Tell daqser to record/send data from these boards
-  timerGroup.AddTimer(100, recordData);
+    Serial.begin(9600);
+    g_comInterface.initialize();
 }
 
 void loop()
 {
-  timerGroup.Tick(millis());
+    g_comInterface.sendMessage(
+        wircom::Message::createMetaMessageResponse(
+            SCHEMA_NAME,
+            SCHEMA_VERSION
+    ));
+
+    delay(1000);
 }
